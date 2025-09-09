e13f337485c926060d01348b4aa8dfaf3fce1ee3
Kai-Doh <github@kaidoh.ch>
2025-09-09 11:24:52 +0200

    Structures added

--- include/structures.h
+++ include/structures.h
@@ -6,17 +6,117 @@
 /*   By: ktiomico <marvin@42lausanne.ch>            +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2025/09/09 09:42:53 by ktiomico          #+#    #+#             */
-/*   Updated: 2025/09/09 09:48:48 by ktiomico         ###   ########.fr       */
+/*   Updated: 2025/09/09 10:21:08 by ktiomico         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #ifndef STRUCTURES_H
 # define STRUCTURES_H
 
+/* ============ BASIC TYPES ============ */
+
 typedef enum e_bool
 {
 	FALSE = 0,
 	TRUE = 1
 }	t_bool;
+
+/* ============ MATHEMATICAL STRUCTURES ============ */
+
+typedef struct s_vector
+{
+	double	x;
+	double	y;
+	double	z;
+}	t_vector;
+
+typedef struct s_color
+{
+	double	r;
+	double	g;
+	double	b;
+}	t_color;
+
+typedef struct s_ray
+{
+	t_vector	origin;
+	t_vector	direction;
+}	t_ray;
+
+/* ============ SCENE ELEMENTS ============ */
+
+typedef struct s_camera
+{
+	t_vector	position;
+	t_vector	orientation;
+	double		fov;
+}	t_camera;
+
+typedef struct s_ambient
+{
+	double	ratio;
+	t_color	color;
+}	t_ambient;
+
+typedef struct s_light
+{
+	t_vector	position;
+	double		brightness;
+	t_color		color;
+}	t_light;
+
+/* ============ GEOMETRIC OBJECTS ============ */
+
+typedef struct s_sphere
+{
+	t_vector	center;
+	double		radius;
+	t_color		color;
+}	t_sphere;
+
+typedef struct s_plane
+{
+	t_vector	point;
+	t_vector	normal;
+	t_color		color;
+}	t_plane;
+
+typedef struct s_cylinder
+{
+	t_vector	center;
+	t_vector	axis;
+	double		radius;
+	double		height;
+	t_color		color;
+}	t_cylinder;
+
+/* ============ SCENE CONTAINER ============ */
+
+typedef struct s_scene
+{
+	t_camera	camera;
+	t_ambient	ambient;
+	t_light		light;
+	t_sphere	*spheres;
+	t_plane		*planes;
+	t_cylinder	*cylinders;
+	int			sphere_count;
+	int			plane_count;
+	int			cylinder_count;
+}	t_scene;
+
+/* ============ PROGRAM DATA ============ */
+
+typedef struct s_data
+{
+	void	*mlx;
+	void	*window;
+	void	*img;
+	char	*addr;
+	int		bpp;
+	int		line_len;
+	int		endian;
+	t_scene	scene;
+}	t_data;
 
 #endif
