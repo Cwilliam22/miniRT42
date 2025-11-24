8da96bc883ac6e4b0d5e43f403fab3f623f4b669
William C <168479898+Cwilliam22@users.noreply.github.com>
2025-11-24 15:56:53 +0100

    bus error...

--- src/raytracing/pixel.c
+++ src/raytracing/pixel.c
@@ -18,10 +18,18 @@ int parse_pixel(t_scene *scene, t_ray *ray)
 			// Initialize ray with origin and direction for each pixel
 			ray->origin = scene->camera.position;
 			// Position pixel with camera's view
+            printf("------------------------------------------------\n");
+            printf("width: %.2f, height: %.2f, px_x: %.2f, px_y: %.2f\n",
+                scene->viewport->width, scene->viewport->height,
+                scene->viewport->px_x, scene->viewport->px_y);
+            printf("------------------------------------------------\n");
             u = -scene->viewport->width  * 0.5 + (x + 0.5) * scene->viewport->px_x;
 			v =  scene->viewport->height * 0.5 - (y + 0.5) * scene->viewport->px_y;
 			w =  scene->camera.d;
 			// Position pixel with world's view
+            //printf("------------------------------------------------\n");
+            //printf("u: %.2f, v: %.2f, w: %.2f\n", u, v, w);
+            //printf("------------------------------------------------\n");
 			ray->direction = vector_normalize(change_base_ray((t_vector){u, v, w}, scene));
 			ray->pixel_x = x;
 			ray->pixel_y = y;
